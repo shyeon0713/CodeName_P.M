2d12fbeafff2a94fae425d88900378cc01e990a9
bottle8429 <134829399+shyeon0713@users.noreply.github.com>
2025-03-11 17:18:46 +0900

    0311
    1. 화질 개선 방법 찾아보기 -> high퀄로 세팅하고 빌드해봐도 구린 화질은 똑같아서 아마 저의 똥컴이 문제인것 같아요ㅜㅜ 일단 화면 크기가 달라져도 비율은 동일하게 유지하는 함수도 추가해야해서 좀 더 찾아볼께요
    2. 버튼 부분 -> 호버효과 및 UI배치 레퍼런스가 좀 더 필요할 것 같음
    3. 3월달 까지 기본적인 게임 로직 및 기획서 작성해야 함

--- Assets/Script/Button/MoveToBriefingRoom.cs
+++ Assets/Script/Button/MoveToBriefingRoom.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MoveToBriefingRoom : MonoBehaviour
 {
-  
-    // 버튼 클릭 시 호출될 메소드
-    public void OnButtonClick()
+
+    private void Update()
     {
-        // 'TargetScene'은 이동하고자 하는 씬의 이름입니다.
-        // 씬 이름을 정확히 입력하세요.
-        SceneManager.LoadScene("BriefingRoom");
+        OnSingleTouch();
     }
+    private void OnSingleTouch()
+    {
+        if (Input.touchCount > 0)  //현재 터치한 손가락 개수가 1개 이상일때 (터치 및 입력이 있을 경우)
+        {
+            Touch touch = Input.GetTouch(0); //-> 단일 터치이기 때문
+
+            if (touch.phase == TouchPhase.Began)   // 터치의 상태가 터치 시작일 때 -> Touch Begin 출력
+            {
+                // 'TargetScene'은 이동하고자 하는 씬의 이름입니다.
+                // 씬 이름을 정확히 입력하세요.
+                SceneManager.LoadScene("BriefingRoom");
+            }
+
+
+            else if (touch.phase == TouchPhase.Ended)   // 터치의 상태가 터치 종료일 ㄸ때 -> Touch End 출력
+            {
+               
+            }
+
+        }
+    }
+ 
 }
--- Assets/Script/Button/MoveToMainRoom.cs
+++ Assets/Script/Button/MoveToMainRoom.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MoveToMainRoom : MonoBehaviour
 {
-  
-    // 버튼 클릭 시 호출될 메소드
-    public void OnButtonClick()
+
+    private void Update()
     {
-        // 'TargetScene'은 이동하고자 하는 씬의 이름입니다.
-        // 씬 이름을 정확히 입력하세요.
-        SceneManager.LoadScene("MainRoom");
+        OnSingleTouch();
+    }
+
+    // 버튼 클릭 시 호출될 메소드
+    private void OnSingleTouch()
+    {
+        if (Input.touchCount > 0)  //현재 터치한 손가락 개수가 1개 이상일때 (터치 및 입력이 있을 경우)
+        {
+            Touch touch = Input.GetTouch(0); //-> 단일 터치이기 때문
+
+            if (touch.phase == TouchPhase.Began)   // 터치의 상태가 터치 시작일 때 -> Touch Begin 출력
+            {
+                // 'TargetScene'은 이동하고자 하는 씬의 이름입니다.
+                // 씬 이름을 정확히 입력하세요.
+                SceneManager.LoadScene("MainRoom");
+            }
+
+
+            else if (touch.phase == TouchPhase.Ended)   // 터치의 상태가 터치 종료일 ㄸ때 -> Touch End 출력
+            {
+                // 효과 입력
+            }
+
+        }
+      
     }
 }
