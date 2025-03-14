0f90c184898a9dd06b0a5d8f246ebe41d8673c33
bottle8429 <134829399+shyeon0713@users.noreply.github.com>
2025-03-14 16:05:32 +0900

    0314
    1. 제작 기획서 작성하면서 어떤 방식으로 구현할 지 같이 작성
    2. .Ui 효과 부분 레퍼런스 계속 찾기 + UI 제작 공부는 계속 진행해야 함
    3. 프리핑 룸 수정 버전 받으면 이미지 교체 후 시스템 설정 버튼 제작

--- Assets/Script/Button/MoveToBriefingRoom.cs
+++ Assets/Script/Button/MoveToBriefingRoom.cs
@@ -2,35 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MoveToBriefingRoom : MonoBehaviour
+using UnityEngine.UI;
+
+public class MovetoBriefingRoom : MonoBehaviour
 {
+    public Button SwitchScene;  // 생성한 버튼 컴포넌트를 연결해 둘 변수
 
-    private void Update()
+    public void Start()
     {
-        OnSingleTouch();
+        SwitchScene = GetComponent<Button>();
+        SwitchScene.onClick.AddListener(ClickCloset);
+
     }
-    private void OnSingleTouch()
+    private void ClickCloset()   //책장 버튼 클릭 시
     {
-        if (Input.touchCount > 0)  //현재 터치한 손가락 개수가 1개 이상일때 (터치 및 입력이 있을 경우)
-        {
-            Touch touch = Input.GetTouch(0); //-> 단일 터치이기 때문
-
-            if (touch.phase == TouchPhase.Began)   // 터치의 상태가 터치 시작일 때 -> Touch Begin 출력
-            {
-                //UI animation 실행
-                //2초 정지
-                // 'TargetScene'은 이동하고자 하는 씬의 이름입니다.
-                // 씬 이름을 정확히 입력하세요.
-                SceneManager.LoadScene("BriefingRoom");
-            }
-
-
-            else if (touch.phase == TouchPhase.Ended)   // 터치의 상태가 터치 종료일 ㄸ때 -> Touch End 출력
-            {
-               
-            }
-
-        }
-    }
+        Debug.Log("ButtonClick");   
+        SceneManager.LoadScene("BriefingRoom");
+    }  
  
 }
