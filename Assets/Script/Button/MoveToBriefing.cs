f1029fe7552552f232d68db5b577fca03e3b4ca7
bottle8429 <134829399+shyeon0713@users.noreply.github.com>
2025-03-15 19:03:10 +0900

    0315
    1.  화질 개선, 화면 비율 전부 완료
    2. 씬이동 전부 구현 완료 -> 추후 UI 효과는 찾은 레퍼런스 참고하여 제작 예정

--- Assets/Script/Button/MoveToBriefing.cs
+++ Assets/Script/Button/MoveToBriefing.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class MoveToOffice : MonoBehaviour
+public class MoveToBriefin : MonoBehaviour
 {
     public Button SwitchScene;  // 생성한 버튼 컴포넌트를 연결해 둘 변수
+
     public void Start()
     {
         SwitchScene = GetComponent<Button>();
-        SwitchScene.onClick.AddListener(GotoOffice);
+        SwitchScene.onClick.AddListener(GotoBriefingScene);
 
     }
-    private void GotoOffice()   //소파 버튼 클릭 시
+    private void GotoBriefingScene()   //책장 버튼 클릭 시
     {
-        //Debug.Log("ButtonClick");
-        SceneManager.LoadScene("Office");
+        // Debug.Log("ButtonClick");   
+        SceneManager.LoadScene("Briefing");
     }
 
 }
