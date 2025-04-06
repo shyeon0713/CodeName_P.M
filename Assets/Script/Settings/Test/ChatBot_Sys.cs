6634118985e51681929be5313013586ed20fe18f
Kimarv <claire2939@gmail.com>
2025-04-06 11:53:54 +0900

    0406
    1. 파일 정리

--- Assets/Script/Settings/Test/ChatBot_Sys.cs
+++ Assets/Script/Settings/Test/ChatBot_Sys.cs
@@ -2,10 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+<<<<<<< Updated upstream
 using UnityEngine;
 
 public class ChatBot_Sys : MonoBehaviour
 {
+=======
+using
+
+public class ChatBot_Sys : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+>>>>>>> Stashed changes
     public void set_Dialog(string scnario_name)
     {
 
